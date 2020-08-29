57465d3a37b94d2be22136ff272c7b46a821f87d
Oshiba Junki <oshiba@m.cs.osakafu-u.ac.jp>
2020-08-29 15:47:49 +0900

    second deploy

--- config/wsgi.py
+++ config/wsgi.py
@@ -7,7 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-from whitenoise.django import DjangoWhiteNoise
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -16,4 +15,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 application = get_wsgi_application()
 
+from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
