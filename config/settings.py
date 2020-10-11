3b97c8e69f658c12ea6d01e378a6cec8e23cfb02
Oshiba Junki <oshiba@m.cs.osakafu-u.ac.jp>
2020-10-11 17:13:24 +0900

    settingsを修正

--- config/settings.py
+++ config/settings.py
@@ -20,11 +20,14 @@ PROJECT_NAME = os.path.basename(BASE_DIR)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '()clc&!m37po74j)o!8qwskd9+48ux3-w3l$^&c@jvtvey(7zy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
